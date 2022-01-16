24715d74c407fd942cce30e4653902fcba2090cd
Deeplobo <deeploobo@gmail.com>
2022-01-16 18:51:32 +0530

    Update algo.js

--- algo.js
+++ algo.js
@@ -1,13 +1,12 @@
-
 function repdigit(a) {
     const length = a.toString().length;
-    let number = a; 
-    for(let i = 0; i< length - 1 ; i++){
+    let number = a;
+    for (let i = 0; i < length - 1; i++) {
         let last = number % 10;
         number = ((number - last) / 10);
         let previousLast = number % 10;
-        if(last !== previousLast){
-            console.log('No Repdigit!');
+        if (last !== previousLast) {
+            console.log('No Repdigit! hello testign');
             return;
         }
     }
